3ed971ec248867c86ae9ec7238d3b4eab2fe25aa
rafaelbtorres <rafaelbartorres@gmail.com>
2021-08-10 16:12:45 -0300

    realocação de arquivos

---
+++ frontend/js/servicos.js
@@ -0,0 +1,31 @@
+var popUpObj;
+function showModalPopUp1(){
+	popUpObj=window.open("https://calendly.com/pampa-barber/corte-de-cabelo","ModalPopUp", "toolbar=no," +"scrollbars=no," + "location=no," + "statusbar=no," + "menubar=no," +"resizable=0," +"width=400," + "height=650");
+	popUpObj.focus();
+	LoadModalDiv();
+}
+function showModalPopUp2(){
+	popUpObj=window.open("https://calendly.com/pampa-barber/ajuste-de-barba","ModalPopUp", "toolbar=no," +"scrollbars=no," + "location=no," + "statusbar=no," + "menubar=no," +"resizable=0," +"width=400," + "height=650");
+	popUpObj.focus();
+	LoadModalDiv();
+}
+function showModalPopUp3(){
+	popUpObj=window.open("https://calendly.com/pampa-barber/penteado-personalizado","ModalPopUp", "toolbar=no," +"scrollbars=no," + "location=no," + "statusbar=no," + "menubar=no," +"resizable=0," +"width=400," + "height=650");
+	popUpObj.focus();
+	LoadModalDiv();
+}
+function showModalPopUp4(){
+	popUpObj=window.open("https://calendly.com/pampa-barber/corte-infantil","ModalPopUp", "toolbar=no," +"scrollbars=no," + "location=no," + "statusbar=no," + "menubar=no," +"resizable=0," +"width=400," + "height=650");
+	popUpObj.focus();
+	LoadModalDiv();
+}
+function showModalPopUp5(){
+	popUpObj=window.open("https://calendly.com/pampa-barber/tratamento","ModalPopUp", "toolbar=no," +"scrollbars=no," + "location=no," + "statusbar=no," + "menubar=no," +"resizable=0," +"width=400," + "height=650");
+	popUpObj.focus();
+	LoadModalDiv();
+}
+function showModalPopUp6(){
+	popUpObj=window.open("https://calendly.com/pampa-barber/home-service","ModalPopUp", "toolbar=no," +"scrollbars=no," + "location=no," + "statusbar=no," + "menubar=no," +"resizable=0," +"width=400," + "height=650");
+	popUpObj.focus();
+	LoadModalDiv();
+}
