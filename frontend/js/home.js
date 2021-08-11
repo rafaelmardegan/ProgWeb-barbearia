fa08dcc945cc0a95d07c1c7f94bf98889a957b0f
glener <glener17@hotmail.com>
2021-08-10 21:05:05 -0300

    a

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,16 +1,20 @@
 function addButton(){
      var btn = document.createElement('Btn');
      btn.classList.add('btn');
-     btn.classList.add('btn-primary');
+     btn.classList.add('btn-light');
      btn.classList.add('btn-lg');
 
      var lbl = document.createTextNode("js_button");   
      btn.appendChild(lbl);   
 
-     btn.onclick = function(){
-         alert('JS ESTA FUNCIONANDO')
-     };
-       
-
     document.getElementsByClassName("add_buttons")[0].append(btn);
+}
+
+
+function alertButton(){
+    alert('JS ESTA FUNCIONANDO')
+}
+
+function returnButton(){
+    window.scrollTo(0,0);
 }
