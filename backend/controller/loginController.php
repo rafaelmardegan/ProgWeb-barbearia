def11c3d87b3354a53354b71f995d6b22ee67bdb
rafael <rafaelmardegan89@gmail.com>
2021-09-24 00:03:00 -0300

    Refatorando index para github pages

--- backend/controller/loginController.php
+++ backend/controller/loginController.php
@@ -6,5 +6,5 @@ if($_POST['usuario'] == 'mardegan' && $_POST['senha'] == '123'){
     $_SESSION['usuario'] = $_POST['usuario'];
     header('Location: ../../view/home.php');
 }else{
-    header('Location: ../../index.php?erro=erroLogin');
+    header('Location: ../../login.php?erro=erroLogin');
 }
--- view/contato.php
+++ view/contato.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(!isset($_SESSION['usuario'])){
-	header('Location: ../index.php?erro=naoAutorizado');
+	header('Location: ../login.php?erro=naoAutorizado');
 	exit;
 }
 ?>
